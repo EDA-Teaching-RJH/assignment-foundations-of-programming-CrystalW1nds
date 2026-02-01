2e5c972029dfb38e04b62596f6bb4b489dac5002
CrystalW1nds <hf280@kent.ac.uk>
2026-02-01 16:44:05 +0000

    added init_database()

---
+++ fleet_manager.py
@@ -0,0 +1,7 @@
+def init_database():
+    name = ["Picard", "Riker", "Data", "Worf", "Kirk"]
+    rank = ["Captain", "Commander", "Lt. Commander", "Lieutenant", "Captain"]
+    division = ["Command", "Command", "Operations", "Security", "Command"]
+    id = ["01", "02", "03", "04", "05"]
+
+    return name, rank, division, id
