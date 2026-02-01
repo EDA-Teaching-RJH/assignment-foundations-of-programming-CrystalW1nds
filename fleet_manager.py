601bfb18e6a840a662274a2925677d04d3df73f5
CrystalW1nds <hf280@kent.ac.uk>
2026-02-01 17:00:01 +0000

    display_menu() function finished

--- fleet_manager.py
+++ fleet_manager.py
@@ -5,3 +5,19 @@ def init_database():
     id = ["01", "02", "03", "04", "05"]
 
     return name, rank, division, id
+
+def display_menu():
+    uName = input("Please enter your full name: ")
+
+    print(uName, " logged in!")
+    print("\n--- MENU ---")
+    print("1) View Crew Members")
+    print("2) Add Crew Member")
+    print("3) Remove Crew Member")
+    print("4) Analyse Data")
+    print("5) Exit")
+
+    opt = input("Select option: ")
+
+    return opt
+
